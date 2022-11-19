c9516bbff2d01a502e7673404b986871aa122ca8
Identityless <rlandwnd555@naver.com>
2022-11-20 03:38:57 +0900

    feat : 컨테이너 네트워크 상태, 호스트 유휴 cpu, memory 정보 추가

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -7,10 +7,9 @@ import com.sun.management.OperatingSystemMXBean;
 import org.json.simple.JSONObject;
 
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.lang.management.ManagementFactory;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
@@ -20,24 +19,44 @@ public class Main {
     private static OperatingSystemMXBean operatingSystemMXBean = ManagementFactory.getPlatformMXBean(OperatingSystemMXBean.class);
     private static List<Map<String, Object>> conDatas;
     private static List<JSONObject> conJson;
-    private static long Rx = -1;
-    private static long Tx = -1;
-    public static void main(String[] args) throws JsonProcessingException {
+    private static NetworkInfo[] networkInfos = new NetworkInfo[100];
+    private static double cpuUsageofAllContainers;
+    private static long momoryUsageofAllContainers;
+    private static String path = "/home/identityless/result.txt";
+    public static void main(String[] args) throws IOException {
         String cmd = "curl -s --unix-socket /var/run/docker.sock http://v1.41/containers/json";
+        PrintWriter printWriter = new PrintWriter(new FileWriter(path, true));
         String jsonString = getJsonString(cmd, true);
         int count;
+        List<JSONObject> conjsonObjectList = null;
         conDatas = objectMapper.readValue(jsonString, new TypeReference<List<Map<String, Object>>>() {});
         //System.out.println(conDatas);
 
-        System.out.println("--------------------Monitoring--------------------");
-        getHostinfo();
+        while(true) {
+            conjsonObjectList = new ArrayList<>();
+            System.out.println("--------------------Monitoring Start--------------------");
+            getHostinfo();
 
-        count = 0;
-        for(Map<String, Object> data : conDatas){
-            System.out.println("------------Container"+count+"------------");
-            getContainerinfo(data);
-            System.out.println("----------------------------------");
-            count++;
+            count = 0;
+            cpuUsageofAllContainers = 0;
+            momoryUsageofAllContainers = 0;
+            for (Map<String, Object> data : conDatas) {
+                System.out.println("------------Container" + count + "------------");
+                conjsonObjectList.add(getContainerinfo(data, count));
+
+                System.out.println("----------------------------------");
+                count++;
+            }
+            jsonObject.put("containers", conjsonObjectList);
+            System.out.println("CPU Available : "+((Runtime.getRuntime().availableProcessors()/2*100)-cpuUsageofAllContainers)+" %");
+            jsonObject.put("cpu_available", ((Runtime.getRuntime().availableProcessors()/2*100)-cpuUsageofAllContainers));
+            System.out.println("Memory Available : "+(operatingSystemMXBean.getTotalPhysicalMemorySize()-momoryUsageofAllContainers)+" bytes");
+            jsonObject.put("memory_available", (operatingSystemMXBean.getTotalPhysicalMemorySize()-momoryUsageofAllContainers));
+            System.out.println("-------------------Momitoring End-------------------\n");
+            System.out.println(jsonObject.toJSONString());
+            printWriter.println(jsonObject.toJSONString());
+            printWriter.flush();
+            printWriter.close();
         }
 
     }
@@ -51,14 +70,19 @@ public class Main {
         System.out.println("whole_mem : "+mem+" bytes");
     }
 
-    private static void getContainerinfo(Map<String, Object> data) throws JsonProcessingException {
+    private static JSONObject getContainerinfo(Map<String, Object> data, int count) throws JsonProcessingException {
         String conId = data.get("Id").toString();
         String conName = data.get("Names").toString();
         conName = conName.substring(2, conName.length()-1);
         String cmd = "curl -s --unix-socket /var/run/docker.sock http://v1.41/containers/"+conName+"/stats";
         String statString = getJsonString(cmd, false);
-
         Map<String, Object> statJson = objectMapper.readValue(statString, new TypeReference<Map<String, Object>>() {});
+        cmd = "curl -s --unix-socket /var/run/docker.sock http://v1.41/containers/"+conName+"/top";
+        statString = getJsonString(cmd, false);
+        Map<String, Object> topJson = objectMapper.readValue(statString, new TypeReference<Map<String, Object>>() {});
+        List<List<Integer>> topList = (List<List<Integer>>) topJson.get("Processes");
+        List<Integer> topRaw = topList.get(0);
+
 
         Map<String, Object> networkInfo = (Map<String, Object>) data.get("NetworkSettings");
         networkInfo = (Map<String, Object>) networkInfo.get("Networks");
@@ -67,14 +91,17 @@ public class Main {
 
         JSONObject conJsonObject = new JSONObject();
 
+        System.out.println("PID : "+topRaw.get(1));
+        conJsonObject.put("pid", topRaw.get(1));
+
         System.out.println("IPAddress : "+networkInfo.get("IPAddress").toString());
         conJsonObject.put("ip_address", networkInfo.get("IPAddress").toString());
 
         System.out.println("Names : "+conName);
         conJsonObject.put("name", conName);
 
-        System.out.println("Process ID : " +conId);
-        conJsonObject.put("process_id", conId);
+        System.out.println("Container ID : " +conId);
+        conJsonObject.put("container_id", conId);
 
         int online_cpus = Integer.parseInt(cpu_stats.get("online_cpus").toString());
         System.out.println("CPU : "+online_cpus);
@@ -96,6 +123,7 @@ public class Main {
 
         System.out.println("CPU Usage % : "+cpu_usage_percent+" %");
         conJsonObject.put("cpu_usage%", cpu_usage_percent);
+        cpuUsageofAllContainers += cpu_usage_percent;
 
 
 //        cpu_delta = cpu_stats.cpu_usage.total_usage - precpu_stats.cpu_usage.total_usage
@@ -116,6 +144,7 @@ public class Main {
 
         System.out.println("Used Memory % : "+(((double)used_memory/available_memory)*100)+" %");
         conJsonObject.put("used_memory_%", (((double)used_memory/available_memory)*100));
+        momoryUsageofAllContainers += used_memory;
 
 
 //        used_memory = memory_stats.usage - memory_stats.stats.cache
@@ -124,17 +153,37 @@ public class Main {
 
         Map<String, Object> network = (Map<String, Object>)statJson.get("networks");
         Map<String, Object> eth0 = (Map<String, Object>) network.get("eth0");
+        NetworkInfo netInfo;
 
 
+        calRxTx(eth0, count);
+        conJsonObject.put("rx_bytes", networkInfos[count].getRxbytes());
+        conJsonObject.put("rx_packets", networkInfos[count].getRxpackets());
+        conJsonObject.put("tx_bytes", networkInfos[count].getTxbytes());
+        conJsonObject.put("tx_packets", networkInfos[count].getTxpackets());
 
-
+        return conJsonObject;
 
 
     }
 
-    private static void calRxTx(Map<String, Object> eth0) {
-        if(Rx == -1) {
-            System.out.println("");
+    private static void calRxTx(Map<String, Object> eth0, int count) {
+        if(networkInfos[count] == null) {
+            networkInfos[count] = new NetworkInfo();
+            networkInfos[count].setRxbytes(Long.parseLong(eth0.get("rx_bytes").toString()));
+            networkInfos[count].setRxpackets(Long.parseLong(eth0.get("rx_packets").toString()));
+            networkInfos[count].setTxbytes(Long.parseLong(eth0.get("tx_bytes").toString()));
+            networkInfos[count].setTxpackets(Long.parseLong(eth0.get("tx_packets").toString()));
+            System.out.println("Rx bytes(packet) : initializing......");
+            System.out.println("Tx bytes(packet) : initializing......");
+        }
+        else {
+            networkInfos[count].setRxbytes(Long.parseLong(eth0.get("rx_bytes").toString())-networkInfos[count].getRxbytes());
+            networkInfos[count].setRxpackets(Long.parseLong(eth0.get("rx_packets").toString())-networkInfos[count].getRxpackets());
+            networkInfos[count].setTxbytes(Long.parseLong(eth0.get("tx_bytes").toString())-networkInfos[count].getTxbytes());
+            networkInfos[count].setTxpackets(Long.parseLong(eth0.get("tx_packets").toString())-networkInfos[count].getTxpackets());
+            System.out.println("Rx bytes(packet) : "+networkInfos[count].getRxbytes()+"("+networkInfos[count].getRxpackets()+")");
+            System.out.println("Tx bytes(packet) : "+networkInfos[count].getTxbytes()+"("+networkInfos[count].getTxpackets()+")");
         }
     }
 
@@ -159,5 +208,51 @@ public class Main {
             System.out.println("IOException occured");
         }
         return jsonString;
+    }
+
+    static class NetworkInfo {
+        private long Rxbytes;
+        private long Txbytes;
+        private long Rxpackets;
+        private long Txpackets;
+
+        public NetworkInfo() {
+            Rxbytes = -1;
+            Txbytes = -1;
+            Rxpackets = -1;
+            Txpackets = -1;
+        }
+
+        public long getRxbytes() {
+            return Rxbytes;
+        }
+
+        public void setRxbytes(long rxbytes) {
+            Rxbytes = rxbytes;
+        }
+
+        public long getTxbytes() {
+            return Txbytes;
+        }
+
+        public void setTxbytes(long txbytes) {
+            Txbytes = txbytes;
+        }
+
+        public long getRxpackets() {
+            return Rxpackets;
+        }
+
+        public void setRxpackets(long rxpackets) {
+            Rxpackets = rxpackets;
+        }
+
+        public long getTxpackets() {
+            return Txpackets;
+        }
+
+        public void setTxpackets(long txpackets) {
+            Txpackets = txpackets;
+        }
     }
 }
